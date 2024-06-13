8fcb4163b418809e8a4d2726c8c84cb4b05a009a
Leroy B <shoked@gmail.com>
2024-06-13 08:47:43 -0500

    Agregar Script de intesrts de Tabla producto

---
+++ Scripts/06092024.sql
@@ -0,0 +1,5 @@
+Insert into Producto (NombreProducto,CategoriaId,MarcaId,Precio,Costo)
+Values ('HP - Envy',1,1,2000, 1500);
+
+Insert into Producto (NombreProducto,CategoriaId,MarcaId,Precio,Costo)
+Values ('HP - EliteBook',1,1,2500, 1500);
